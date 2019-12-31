deba6dcf5c47dd3e6c7eb6bcfec5d8f2fe5f0ae1
rejohnson547 <re.johnson547@gmail.com>
2019-12-30 20:49:50 -0500

    12/30/2019

--- collatz.py
+++ collatz.py
@@ -5,8 +5,15 @@ number = int(input('Please enter a number: '))
 def collatz(number):
     while True:
         if number % 2 == 0:
-            return number // 2
+            answer = number // 2
+            print(answer)
         elif number % 2 == 1:
-            return 3 * number + 1
-    
+            answer = 3 * number + 1
+            print(answer)
+        elif answer != 1:
+            collatz(answer)
+        else:
+            break
+
+        
 print(collatz(number))
