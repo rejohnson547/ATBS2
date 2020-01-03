8c718aaf15f5e02e7099c1759296a85a1435d1a8
rejohnson547 <re.johnson547@gmail.com>
2020-01-02 21:33:05 -0500

    01/02/2020

---
+++ myPets.py
@@ -0,0 +1,9 @@
+# 01/02/2020
+
+my_pets = ['Zophie', 'Pooka', 'Fat-tail']
+print('Enter a pet name:')
+name = input()
+if name not in my_pets:
+    print('I do not have a pet named ' + name)
+else:
+    print(name + ' is my pet.')
