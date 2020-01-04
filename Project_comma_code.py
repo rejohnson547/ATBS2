c40158de7c02e626a652d7c20ccca31c0d16e598
rejohnson547 <re.johnson547@gmail.com>
2020-01-03 22:28:53 -0500

    01/03/2020

--- Project_comma_code.py
+++ Project_comma_code.py
@@ -1,5 +1,17 @@
 # 01/03/2020
 
+"""Comma Code
+
+Say you have a list value like this:
+
+ spam = ['apples', 'bananas', 'tofu', 'cats']
+Write a function that takes a list value as an argument and
+returns a string with all the items separated by a comma and
+a space, with and inserted before the last item. For example,
+passing the previous spam list to the function would return 'apples,
+bananas, tofu, and cats'. But your function should be able to work with any
+list value passed to it."""
+
 spam = ['apples', 'bananas', 'tofu', 'cats']
 games = ['Pokemon Sword/Shield', 'Resident Evil 2', 'Call of Duty', 'Dragon Quest 11']
 
