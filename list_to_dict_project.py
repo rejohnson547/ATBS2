966f76a6d96bb55c2371648943694451fc421399
rejohnson547 <re.johnson547@gmail.com>
2020-01-08 22:17:57 -0500

    not finished yet

---
+++ list_to_dict_project.py
@@ -0,0 +1,28 @@
+# 01/08/2020
+
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = {'gold coin':42, 'rope':1}
+
+
+def add_to_inventory(inventory, added_items):
+    print('Inventory:\n')
+    coins = 0
+    dagger = 0
+    ruby = 0
+    for item in dragon_loot:
+        if 'gold coin' in dragon_loot:
+            coins += 1
+            
+    #for i in dragon_loot:
+        if 'dagger' in dragon_loot:
+            dagger += 1
+    #for v in dragon_loot:
+        if 'ruby' in dragon_loot:
+            ruby += 1
+    print(coins)
+
+add_to_inventory(inv, dragon_loot)
+    
+    
+
+
