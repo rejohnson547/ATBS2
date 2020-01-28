f4c95e0c0ddc1a6797c36ac5625f3d5763d5fb98
rejohnson547 <re.johnson547@gmail.com>
2020-01-27 19:27:54 -0500

    01/27/2020

--- is_phone_number.py
+++ is_phone_number.py
@@ -19,7 +19,11 @@ def is_phone_number(text):
     else:
         return True
 
-print('Is 415-555-4242 a phone number?')
-print(is_phone_number('415-555-4242'))
-print('Is Moshi moshi a phone number?')
-print(is_phone_number('Moshi moshi'))
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i + 12]
+    if is_phone_number(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
+
+print(is_phone_number(message))
